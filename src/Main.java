2ce3568098870a4c7fbe79355849471c2be6fff6
Valeria <v.caro@uniandes.edu.co>
2023-04-11 00:47:38 -0500

    pages

--- src/Main.java
+++ src/Main.java
@@ -5,48 +5,78 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public final static ArrayList<Integer> references = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Write the file name: ");
-        String fileName = sc.nextLine();
-        File file = new File("data/"+fileName);
-        Scanner fileReader = new Scanner(file);
-        List<String> list = new ArrayList<String>();
+        System.out.println("Choose the mode: ");
+        System.out.println("1. Mode 1");
+        System.out.println("2. Mode 2");
+        int mode = sc.nextInt();
+        sc.nextLine();
 
-        while (fileReader.hasNextLine()) {
-           list.add(fileReader.nextLine());
+        if (mode == 1) {
+            ModeOne();
+            System.out.println("Pages reference: "+ references);
+        } else if (mode == 2) {
+            ModeTwo();
+        } else {
+            System.out.println("Invalid mode");
         }
 
-        // Read the first four rows of the file
-        String[] tpsize = list.get(0).split("=");
-        int tp  = Integer.parseInt(tpsize[1]);
-
-        String[] nfsize = list.get(1).split("=");
-        int nf = Integer.parseInt(nfsize[1]);
-
-        String[] ncsize = list.get(2).split("=");
-        int nc = Integer.parseInt(ncsize[1]);
-
-        String[] nrsize = list.get(3).split("=");
-        int nr = Integer.parseInt(nrsize[1]);
-
-        System.out.println("TP size: "+tp);
-        System.out.println("NF size: "+nf);
-        System.out.println("NC size: "+nc);
-        System.out.println("NR size: "+nr);
-
-        // Iterate through the rest of the file and print the values
-        for (int i = 4; i < list.size(); i++) {
-            // split the string by , and get [0]
-            String[] values = list.get(i).split(",");
-            // print the values[0]
-            System.out.println(values[0]);
-        }
-
-        fileReader.close();
         sc.close();
+    }
 
+    public static void ModeOne() {
 
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Write the file name: ");
+            String fileName = sc.nextLine();
+            File file = new File("data/"+fileName + ".txt");
+            Scanner fileReader = new Scanner(file);
+            List<String> list = new ArrayList<String>();
+    
+            while (fileReader.hasNextLine()) {
+               list.add(fileReader.nextLine());
+            }
+    
+            String[] tpsize = list.get(0).split("=");
+            int tp  = Integer.parseInt(tpsize[1]);
+    
+            String[] nfsize = list.get(1).split("=");
+            int nf = Integer.parseInt(nfsize[1]);
+    
+            String[] ncsize = list.get(2).split("=");
+            int nc = Integer.parseInt(ncsize[1]);
+    
+            String[] nrsize = list.get(3).split("=");
+            int nr = Integer.parseInt(nrsize[1]);
+    
+            System.out.println("TP size: "+tp);
+            System.out.println("NF size: "+nf);
+            System.out.println("NC size: "+nc);
+            System.out.println("NR size: "+nr);
+    
+            // Iterate through the rest of the file and print the values
+    
+            for (int i = 4; i < list.size(); i++) {
+                String[] values = list.get(i).split(",");
+                String page = values[1];
+                references.add(Integer.parseInt(page));
+            }
+    
+            fileReader.close();
+            sc.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+        }
+
+    }
+
+    public static void ModeTwo() {
 
     }
 }
